ac94345f9f49d608c5f759a339a06fa1ccf3ad46
yebin <yebind3308@gmail.com>
2025-01-30 22:14:41 +0900

    feat: update date/time, fetch data by user location, add coordinate calculation function

--- app/api/weather/route.js
+++ app/api/weather/route.js
@@ -1,25 +1,41 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 import dotenv from "dotenv";
+import _ from "lodash";
 
 dotenv.config();
 
-export async function GET() {
+export async function GET(req) {
   const apiKey = process.env.WEATHER_API_KEY;
   const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst`;
+  const today = new Date();
+  const base_date =
+    _.padStart(today.getFullYear(), 4, "0") +
+    _.padStart(today.getMonth() + 1, 2, "0") +
+    _.padStart(today.getDate(), 2, "0");
+
+  today.setMinutes(today.getMinutes() - 20);
+  const hours = _.padStart(today.getHours(), 2, "0");
+  const minutes = _.padStart(today.getMinutes(), 2, "0");
+  const base_time = `${hours}${minutes}`;
+
+  const { searchParams } = new URL(req.url);
+  const nx = searchParams.get("nx");
+  const ny = searchParams.get("ny");
 
   const params = new URLSearchParams({
     serviceKey: apiKey,
     pageNo: 1,
     numOfRows: 1000,
     dataType: "JSON",
-    base_date: 20250130,
-    base_time: "0630",
-    nx: 55,
-    ny: 127,
+    base_date: base_date,
+    base_time: base_time,
+    nx: nx,
+    ny: ny,
   });
 
   try {
+    console.log("base_date", base_date, "base_time", base_time, "nx", nx);
     const response = await axios.get(`${url}?${params.toString()}`);
     return NextResponse.json(response.data);
   } catch (error) {
--- app/lib/useGetWeather.tsx
+++ app/lib/useGetWeather.tsx
@@ -1,11 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
+import useLocationStore from "../store/useLocationStore";
+import { dfs_xy_conv } from "../hooks/useCoordinateTransformer";
 
 // 날씨 정보 받아오는 api
 const useGetWeather = () => {
+  const { location } = useLocationStore();
+  const result = dfs_xy_conv("toXY", location.latitude, location.longitude); // 위도, 경도
   const getWeatherQuerykey = ["weather"];
 
   const getWeatherData = async () => {
-    return await fetch("/api/weather", { method: "GET" })
+    return await fetch(`/api/weather?nx=${result.x}&ny=${result.y}`)
       .then((res) => {
         if (!res.ok) throw new Error("Network response was not ok");
         return res.json();
@@ -18,6 +22,7 @@ const useGetWeather = () => {
   const { data: weatherData } = useQuery({
     queryKey: getWeatherQuerykey,
     queryFn: getWeatherData,
+    enabled: !!location.latitude && !!location.longitude,
   });
 
   return { weatherData };
