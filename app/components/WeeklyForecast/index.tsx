2c5976d59e84e1013e6a5d31a059e0fba6d2ca5d
yebin <yebind3308@gmail.com>
2025-02-01 16:07:22 +0900

    feat: Complete UI styling of WeeklyForecast screen

--- app/components/WeeklyForecast/index.tsx
+++ app/components/WeeklyForecast/index.tsx
@@ -7,22 +7,70 @@ import SUN from "/public/weather/sun.png";
 const WeeklyForecast = () => {
   const todayTomorrow = [
     {
-      className: "today",
       day: "오늘",
       date: "1.23",
       minTemperature: "-2",
       maxTemperature: "6",
       morningPrecipitation: "0",
-      afternoonPrecipitation: "6",
+      afternoonPrecipitation: "0",
     },
     {
-      className: "tomorrow",
       day: "내일",
       date: "1.24",
       minTemperature: "-2",
       maxTemperature: "9",
       morningPrecipitation: "0",
-      afternoonPrecipitation: "9",
+      afternoonPrecipitation: "0",
+    },
+  ];
+  const weekForecast = [
+    {
+      day: "토",
+      date: "1.25",
+      minTemperature: "0",
+      maxTemperature: "2",
+      morningPrecipitation: "0",
+      afternoonPrecipitation: "0",
+    },
+    {
+      day: "일",
+      date: "1.26",
+      minTemperature: "-1",
+      maxTemperature: "7",
+      morningPrecipitation: "10",
+      afternoonPrecipitation: "20",
+    },
+    {
+      day: "월",
+      date: "1.26",
+      minTemperature: "-1",
+      maxTemperature: "7",
+      morningPrecipitation: "10",
+      afternoonPrecipitation: "20",
+    },
+    {
+      day: "화",
+      date: "1.26",
+      minTemperature: "-1",
+      maxTemperature: "7",
+      morningPrecipitation: "10",
+      afternoonPrecipitation: "20",
+    },
+    {
+      day: "수",
+      date: "1.26",
+      minTemperature: "-1",
+      maxTemperature: "7",
+      morningPrecipitation: "10",
+      afternoonPrecipitation: "20",
+    },
+    {
+      day: "목",
+      date: "1.26",
+      minTemperature: "-1",
+      maxTemperature: "7",
+      morningPrecipitation: "10",
+      afternoonPrecipitation: "20",
     },
   ];
 
@@ -63,7 +111,33 @@ const WeeklyForecast = () => {
         ))}
       </ul>
 
-      <div className="week-forecast">주간 날씨를 알려주는 컴포넌트</div>
+      <ul className="week-forecast">
+        {weekForecast.map((item) => (
+          <li className="week-forecast-list">
+            <div className="precipitation-info">
+              <span className="day">{item.day}</span>
+              <span className="date">{item.date}</span>
+            </div>
+
+            <div className="weather-icon">
+              <Image className="weather-img" src={SUN} alt="맑음" />
+              <Image className="weather-img" src={SUN} alt="맑음" />
+            </div>
+
+            <div className="week-forecast-detail">
+              <div className="temperature">
+                <span className="min-temperature">{item.minTemperature}</span>
+                <span className="temp-divider">/</span>
+                <span className="max-temperature">{item.maxTemperature}</span>
+              </div>
+              <div className="precipitation-probability">
+                <span>{item.morningPrecipitation}%</span>
+                <span>{item.afternoonPrecipitation}%</span>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
